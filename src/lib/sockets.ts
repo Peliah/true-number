da5279e19c0b1a5324b5cc9f596cae89a18ad55e
Peliah <pelepoupa@gmail.com>
2025-07-28 21:15:00 +0100

    fix: removed sensitive route

--- src/lib/sockets.ts
+++ src/lib/sockets.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 import { getToken } from "./config";
 
-const URL = "http://localhost:3000";
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 
 let socket: Socket | null = null;
 
